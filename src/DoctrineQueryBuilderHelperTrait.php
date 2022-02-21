37e6f41ad58e262d8cbc8cbb42833fb96df5e536
Yelnur Urazymbetov <311293e@gmail.com>
2022-02-21 23:51:48 +0600

    bugfix

--- src/DoctrineQueryBuilderHelperTrait.php
+++ src/DoctrineQueryBuilderHelperTrait.php
@@ -13,7 +13,7 @@ trait DoctrineQueryBuilderHelperTrait {
         QueryBuilder $qb,
         string|int|null $valueType = null,
     ): void {
-        $fieldName = self::getFieldName($field);
+        $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($field);
 
         if ( is_array($value) && count($value) > 1 ) {
             $qb
@@ -40,7 +40,7 @@ trait DoctrineQueryBuilderHelperTrait {
         QueryBuilder $qb,
         string|int|null $valueType = null,
     ): void {
-        $fieldName = self::getFieldName($field);
+        $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($field);
 
         if ( is_array($value) && count($value) > 1 ) {
             $qb
@@ -94,7 +94,7 @@ trait DoctrineQueryBuilderHelperTrait {
             $likes = [];
 
             foreach ($field as $f) {
-                $fieldName = self::getFieldName($f);
+                $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($f);
 
                 $likes[] = $qb->expr()->like(
                     $qb->expr()->lower($f),
@@ -108,7 +108,7 @@ trait DoctrineQueryBuilderHelperTrait {
                 $qb->expr()->orX(...$likes)
             );
         } else {
-            $fieldName = self::getFieldName($field);
+            $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($field);
 
             $qb
                 ->andWhere(
@@ -130,8 +130,8 @@ trait DoctrineQueryBuilderHelperTrait {
         string|int|null $valueType = null,
     ): void {
         if ( $gte !== null && $lte !== null ) {
-            $fieldNameGte = self::getFieldName($field.'_gte');
-            $fieldNameLte = self::getFieldName($field.'_lte');
+            $fieldNameGte = DoctrineQueryBuilderHelperTrait::getFieldName($field.'_gte');
+            $fieldNameLte = DoctrineQueryBuilderHelperTrait::getFieldName($field.'_lte');
 
             $qb
                 ->andWhere(
@@ -149,7 +149,7 @@ trait DoctrineQueryBuilderHelperTrait {
         }
 
         if ( $gte !== null ) {
-            $fieldName = self::getFieldName($field);
+            $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($field);
 
             $qb
                 ->andWhere(
@@ -163,7 +163,7 @@ trait DoctrineQueryBuilderHelperTrait {
         }
 
         if ( $lte !== null ) {
-            $fieldName = self::getFieldName($field);
+            $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($field);
 
             $qb
                 ->andWhere(
@@ -184,7 +184,7 @@ trait DoctrineQueryBuilderHelperTrait {
         QueryBuilder    $qb,
         string|int|null $valueType = null,
     ): void {
-        $fieldName = self::getFieldName($field);
+        $fieldName = DoctrineQueryBuilderHelperTrait::getFieldName($field);
 
         if ( $gte !== null ) {
             $qb
@@ -219,7 +219,7 @@ trait DoctrineQueryBuilderHelperTrait {
 
     private static function getFieldName(string $field): string {
         $field = str_replace('.', '_', $field);
-        $number = (self::$fieldNumber[$field] ?? 0) + 1;
+        $number = (DoctrineQueryBuilderHelperTrait::$fieldNumber[$field] ?? 0) + 1;
 
         return $field . $number;
     }
